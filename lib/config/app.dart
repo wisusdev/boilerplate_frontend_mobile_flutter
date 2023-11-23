95fa0bdd0e9601b3e74dc81b26885b18611f1573
Jesus Avelar <wisusdev@gmail.com>
2023-11-22 21:50:55 -0600

    Refactor app configuration

--- lib/config/app.dart
+++ lib/config/app.dart
@@ -1,17 +1,11 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class App {
+// Titulo de la app
+final appName = dotenv.get('APP_NAME');
 
-    static String get appName {
-        return dotenv.get('APP_NAME');
-    }
+// Version de la app
+final appVersion = dotenv.get('APP_VERSION');
 
-    static String get appVersion {
-        return dotenv.get('APP_VERSION');
-    }
-
-    static String get appLanguage {
-        return dotenv.get('APP_LANGUAGE');
-    }
-
-}
+// Soporte para idiomas
+final appLang = dotenv.get('APP_LANG');
+final appLangCountry = dotenv.get('APP_LANG_COUNTRY');
