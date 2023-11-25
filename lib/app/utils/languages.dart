131b71998cdf22a412c70dd1db9a6a4136986122
Jesus Avelar <wisusdev@gmail.com>
2023-11-24 21:57:03 -0600

    Add language models to languages.dart

---
+++ lib/app/utils/languages.dart
@@ -0,0 +1,18 @@
+import 'package:todolist_flutter/app/models/LanguagesModel.dart';
+
+List<LanguageModel> languages = <LanguageModel>[
+
+	LanguageModel(
+		imageUrl: '',
+		languageName: 'English',
+		countryCode: 'US',
+		languageCode: 'en'
+	),
+
+	LanguageModel(
+		imageUrl: '',
+		languageName: 'Español',
+		countryCode: 'De',
+		languageCode: 'es'
+	),
+];
