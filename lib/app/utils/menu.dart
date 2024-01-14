6577868d71dcea7838c50953c461f35e4617ab15
Jesus Avelar <wisusdev@gmail.com>
2024-01-13 23:12:19 -0600

    Remove unused menu items

--- lib/app/utils/menu.dart
+++ lib/app/utils/menu.dart
@@ -10,26 +10,12 @@ const appMenuItems = <MenuItem>[
 		icon: Icons.home
 	),
 
-    MenuItem(
-		title: 'sign_in', 
-		subTitle: 'Login view', 
-		link: 'login', 
-		icon: Icons.login
-	),
-
 	MenuItem(
 		title: 'profile', 
 		subTitle: 'Profile view', 
 		link: 'profile', 
 		icon: Icons.person
 	),
-
-    MenuItem(
-        title: 'sign_up', 
-        subTitle: 'Register view', 
-        link: 'register', 
-        icon: Icons.app_registration
-    ),
 
 	MenuItem(
 		title: 'settings', 
