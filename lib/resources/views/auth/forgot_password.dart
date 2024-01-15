66f1a798909a9b650ff43e13a0408eddab3a3262
Jesus Avelar <wisusdev@gmail.com>
2024-01-14 22:28:41 -0600

    Refactor forgot password UI

--- lib/resources/views/auth/forgot_password.dart
+++ lib/resources/views/auth/forgot_password.dart
@@ -37,58 +37,63 @@ class _AuthForgotPasswordState extends State<AuthForgotPassword> {
       		appBar: AppBar(
         		title: const Text('Forgot Password'),
       		),
-      		body: Padding(
-				padding: EdgeInsets.only(top: sizeWith * 0.05, bottom: 0, left: 20, right: 20),
-        		child: Form(
-					key: _formKey,
-					child: Column(
-						//mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-						children: [
-							const Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
+      		body: Center(
+                child: Container(
+                    constraints: const BoxConstraints(maxWidth: 400),
+                    child: Padding(
+                        padding: EdgeInsets.only(top: sizeWith * 0.05, bottom: 0, left: 20, right: 20),
+                        child: Form(
+                            key: _formKey,
+                            child: Column(
+                                //mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                    Image(
-                                        image: AssetImage('lib/assets/images/kyan.png'), 
-                                        width: 80,
-                                        height: 80,
+                                    const Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                            Image(
+                                                image: AssetImage('lib/assets/images/wisus-logo.png'), 
+                                                width: 80,
+                                                height: 80,
+                                            ),
+                                        ],
+                                    ),
+
+                                    const SizedBox(height: 20),
+
+                                    Text(Location.of(context)!.trans('forget_password_instructions'), style: const TextStyle( fontSize: 16)),
+
+                                    const SizedBox(height: 20),
+
+                                    TextField(
+                                        controller: _emailController,
+                                        decoration: InputDecoration(
+                                            labelText: Location.of(context)!.trans('email'),
+                                            prefixIcon: const Icon(Icons.email_outlined),
+                                            border: const OutlineInputBorder(),
+                                        ),
+                                        onChanged: _validateEmail,
+                                    ),
+
+                                    const SizedBox(height: 30),
+                                    ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Theme.of(context).colorScheme.onPrimary,
+                                            foregroundColor: Theme.of(context).colorScheme.primary,
+                                            minimumSize: const Size.fromHeight(45),
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(20),
+                                            ),
+                                        ),
+                                        onPressed: _isEmailValid ? () {} : null,
+                                        child: Text(Location.of(context)!.trans('reset_pwd')),
                                     ),
                                 ],
                             ),
-
-							const SizedBox(height: 20),
-
-							Text(Location.of(context)!.trans('forget_password_instructions'), style: const TextStyle( fontSize: 16)),
-
-							const SizedBox(height: 20),
-
-							TextField(
-								controller: _emailController,
-								decoration: InputDecoration(
-									labelText: Location.of(context)!.trans('email'),
-									prefixIcon: const Icon(Icons.email_outlined),
-									border: const OutlineInputBorder(),
-								),
-								onChanged: _validateEmail,
-							),
-
-							const SizedBox(height: 30),
-							ElevatedButton(
-								style: ElevatedButton.styleFrom(
-                                    backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                                    foregroundColor: Theme.of(context).colorScheme.primary,
-                                    minimumSize: const Size.fromHeight(45),
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(20),
-                                    ),
-                                ),
-								onPressed: _isEmailValid ? () {} : null,
-								child: Text(Location.of(context)!.trans('reset_pwd')),
-							),
-						],
-					),
-				),
-      		),
+                        ),
+                    ),
+                ),
+            ),
     	);
   	}
 }
