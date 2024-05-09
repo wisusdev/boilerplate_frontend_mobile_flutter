59163ddbb55d582c92ce19d345772a44fd678c17
Jesús Avelar <wisusdev@gmail.com>
2024-05-08 23:03:53 -0600

    Update services configuration to remove dependency on dotenv package

--- lib/config/services.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-final Map<String, String> todoListService = {
-    'url': dotenv.get('API_URL'),
-    'api_version': dotenv.get('API_VERSION'),
-};
