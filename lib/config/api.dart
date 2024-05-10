ad1c7226788471fb80c70471ccefdd357f45cfb1
Jesús Avelar <wisusdev@gmail.com>
2024-05-09 23:52:50 -0600

    Update API configuration variable names to follow camel case convention

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-final String api_url = dotenv.get('API_URL');
-final String api_version = dotenv.get('API_VERSION');
+final String apiUrl = dotenv.get('API_URL');
+final String apiVersion = dotenv.get('API_VERSION');
 
-final api_url_v1 = '$api_url/$api_version';
+final String apiUrlV1 = '$apiUrl/$apiVersion';
