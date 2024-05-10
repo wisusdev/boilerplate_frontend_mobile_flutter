2b2a56036399155c9ed75cff678bb2f28e227f0f
Jesús Avelar <wisusdev@gmail.com>
2024-05-10 12:45:53 -0600

    feat: Refactor ProfileService to use UserModel interface

--- lib/app/services/profile_service.dart
+++ lib/app/services/profile_service.dart
@@ -2,32 +2,32 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:todolist_flutter/app/models/user_model.dart';
+import 'package:todolist_flutter/app/interfaces/response/user_model.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ProfileService {
-  final storage = const FlutterSecureStorage();
+    final storage = const FlutterSecureStorage();
 
-  late String _apiUri;
-  late String _apiUserInfo;
+    late String _apiUri;
+    late String _apiUserInfo;
 
-  Map<String, String> headers = {'Content-Type': 'application/json'};
+    Map<String, String> headers = {'Content-Type': 'application/json'};
 
-  ProfileService() {
-    _apiUri = dotenv.get('API_URL') + dotenv.get('API_VERSION');
-    _apiUserInfo = '$_apiUri/user';
-  }
-
-  Future<UserModel> userInfo() async {
-    var uri = Uri.parse(_apiUserInfo);
-    var token = await storage.read(key: 'token');
-    headers['Authorization'] = 'Bearer $token';
-    var response = await http.get(uri, headers: headers);
-
-    if (response.statusCode == 200) {
-      return UserModel.fromJson(json.decode(response.body));
-    } else {
-      throw Exception('Failed to load user info');
+    ProfileService() {
+        _apiUri = dotenv.get('API_URL') + dotenv.get('API_VERSION');
+        _apiUserInfo = '$_apiUri/user';
     }
-  }
+
+    Future<UserModel> userInfo() async {
+        var uri = Uri.parse(_apiUserInfo);
+        var token = await storage.read(key: 'token');
+        headers['Authorization'] = 'Bearer $token';
+        var response = await http.get(uri, headers: headers);
+
+        if (response.statusCode == 200) {
+            return UserModel.fromJson(json.decode(response.body));
+        } else {
+            throw Exception('Failed to load user info');
+        }
+    }
 }
