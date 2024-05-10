4f027610d9061aa1e8689b01c631c9bf22a27cf7
Jesús Avelar <wisusdev@gmail.com>
2024-05-09 23:53:11 -0600

    chore: Update API configuration variable names to follow camel case convention

--- lib/resources/views/auth/login.dart
+++ lib/resources/views/auth/login.dart
@@ -44,11 +44,7 @@ class _AuthLoginState extends State<AuthLogin> {
 											const Row(
 												mainAxisAlignment: MainAxisAlignment.center,
 												children: [
-													Image(
-														image: AssetImage(logoApp), 
-														width: 80,
-														height: 80,
-													),
+													Image(image: AssetImage(logoApp), width: 80, height: 80,),
 												],
 											),
 									
@@ -64,123 +60,121 @@ class _AuthLoginState extends State<AuthLogin> {
 												),
 												validator: (value) {
 													if (value!.isEmpty) {
-														return Location.of(context)!.trans('validation.emailRequired');
-													}
+                                                        return Location.of(context)!.trans('validation.emailRequired');
+                                                    }
 
-													if (!RegExp(r'^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
-														return Location.of(context)!.trans('validation.emailEmail');
-													}
+                                                    if (!RegExp(r'^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                                                        return Location.of(context)!.trans('validation.emailEmail');
+                                                    }
+                                                    
+                                                    return null;
+                                                }
+                                            ),
 
-													return null;
-												}
-											),
-									
-											const SizedBox(height: 20),
-										
-											TextFormField(
-												controller: _passwordController,
+                                            const SizedBox(height: 20),
+
+                                            TextFormField(
+                                                controller: _passwordController,
                                                 keyboardType: TextInputType.visiblePassword,
-												decoration: InputDecoration(
-													labelText: Location.of(context)!.trans('password'),  
-													prefixIcon: const Icon(Icons.lock_outline),
-													border: const OutlineInputBorder(),
-												),
-												obscureText: true,
-												validator: (value) {
-													if (value!.isEmpty) {
-														return 'Por favor ingresa tu contraseña';
-													}
+                                                decoration: InputDecoration(
+                                                    labelText: Location.of(context)!.trans('password'),
+                                                    prefixIcon: const Icon(Icons.lock_outline),
+                                                    border: const OutlineInputBorder(),
+                                                ),
+                                                obscureText: true,
+                                                validator: (value) {
+                                                    if (value!.isEmpty) {
+                                                        return 'Por favor ingresa tu contraseña';
+                                                    }
 
-													if (value.length < 8) {
-														return 'La contraseña debe tener al menos 8 caracteres';
-													}
+                                                    if (value.length < 8) {
+                                                        return 'La contraseña debe tener al menos 8 caracteres';
+                                                    }
 
-													return null;
-												}
-											),
-									
-											const SizedBox(height: 30),
-										
-											Row(
-												mainAxisAlignment: MainAxisAlignment.end,
-												children: [
-													InkWell(
-														onTap: () {
-															Navigator.pushNamed(context, 'forgot_password');
-														},
-														child: Text(Location.of(context)!.trans('forgotPassword')),
-													)
-												],
-											),
-										
-											const SizedBox(height: 30),
-										
-											ElevatedButton(
-												style: ElevatedButton.styleFrom(
-													backgroundColor: Theme.of(context).colorScheme.onPrimary,
-													foregroundColor: Theme.of(context).colorScheme.primary,
-													minimumSize: const Size.fromHeight(45),
-													shape: RoundedRectangleBorder(
-														borderRadius: BorderRadius.circular(20),
-													),
-												),
-												onPressed: (){
-													if (_formKey.currentState!.validate()) {
-														_formKey.currentState!.save();
-														
-														login(context);
+                                                    return null;
+                                                }
+                                            ),
+
+                                            const SizedBox(height: 30),
+
+                                            Row(
+                                                mainAxisAlignment: MainAxisAlignment.end,
+                                                children: [
+                                                    InkWell(
+                                                        onTap: () {
+                                                            Navigator.pushNamed(context, 'forgot_password');
+                                                        },
+                                                        child: Text(Location.of(context)!.trans('forgotPassword')),
+                                                    )
+                                                ],
+                                            ),
+
+                                            const SizedBox(height: 30),
+
+                                            ElevatedButton(
+                                                style: ElevatedButton.styleFrom(
+                                                    backgroundColor: Theme.of(context).colorScheme.onPrimary,
+                                                    foregroundColor: Theme.of(context).colorScheme.primary,
+                                                    minimumSize: const Size.fromHeight(45),
+                                                    shape: RoundedRectangleBorder(
+                                                        borderRadius: BorderRadius.circular(20),
+                                                    ),
+                                                ),
+                                                onPressed: () {
+                                                    if (_formKey.currentState!.validate()) {
+                                                        _formKey.currentState!.save();
+
+                                                        login(context);
 
                                                         getScafoldMessage(context, 'Procesando datos...');
-													}
-												}, 
-												child: Text(Location.of(context)!.trans('login'), style: const TextStyle(fontSize: 16))
-											),    
+                                                    }
+                                                },
+                                                child: Text(Location.of(context)!.trans('login'), style: const TextStyle(fontSize: 16))
+                                            ),
 
-											const SizedBox(height: 10),
-										],
-									),
-								),
-							),
-							
-							Positioned(
-								child: Container(
-									padding: EdgeInsets.only(top: sizeWith * 1.30),
-									child: Row(
-										mainAxisAlignment: MainAxisAlignment.center,
-										children: [
-											Text(Location.of(context)!.trans('dontHaveAccount')),
-											InkWell(
-												onTap: () {
-													Navigator.pushNamed(context, 'register');
-												},
-												child: Text(' ${Location.of(context)!.trans('registerHere')}', style: const TextStyle(fontWeight: FontWeight.bold)),
-											),
-										],
-									),
-								)
-							)
-						]
-					),
-				),
-			),
-		);
-  	}
+                                            const SizedBox(height: 10),
+                                        ],
+                                    ),
+                                ),
+                            ),
+
+                            Positioned(
+                                child: Container(
+                                    padding: EdgeInsets.only(top: sizeWith * 1.30),
+                                    child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                            Text(Location.of(context)!.trans('dontHaveAccount')),
+                                            InkWell(
+                                                onTap: () {
+                                                    Navigator.pushNamed(context, 'register');
+                                                },
+                                                child: Text(' ${Location.of(context)!.trans('registerHere')}', style: const TextStyle(fontWeight: FontWeight.bold)),
+                                            ),
+                                        ],
+                                    ),
+                                )
+                            )
+                        ]
+                    ),
+                ),
+            ),
+        );
+    }
 
     void login(context) async {
         Map<String, String> data = {
+            'type': 'users',
             'email': _emailController.text,
             'password': _passwordController.text,
         };
 
-        var response = await AuthService().login(data);
-        var body = json.decode(response.body);
-
-        if (body['status']) {
-            if (!mounted) return;
+        bool loginResponse = await AuthService().login(data: data);
+        print(loginResponse);
+        if(loginResponse){
             Navigator.pushNamed(context, 'home');
         } else {
-            getScafoldMessage(context, body['message']);
-
+            getScafoldMessage(context, 'Error al iniciar sesión');
         }
     }
 }
