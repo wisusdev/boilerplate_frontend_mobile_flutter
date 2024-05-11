14bbcd8ca8cb28da68db413245e6deab5aa600a5
Jesús Avelar <wisusdev@gmail.com>
2024-05-11 09:37:13 -0600

    feat: Update ApiInterceptor to handle JSON API requests and include authorization token

--- lib/app/interceptors/api_interceptor.dart
+++ lib/app/interceptors/api_interceptor.dart
@@ -1,42 +1,45 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiInterceptor extends http.BaseClient {
+    final http.Client _inner = http.Client();
+    final storage = const FlutterSecureStorage();
 
-  final http.Client _inner = http.Client();
-  final storage = const FlutterSecureStorage();
+    @override
+    Future<http.StreamedResponse> send(http.BaseRequest request) async {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        String? token = prefs.getString('access_token');
 
-  @override
-  Future<http.StreamedResponse> send(http.BaseRequest request) async {
+        if (request is http.Request) {
+            if (request.method == 'POST' || request.method == 'PATCH' || request.method == 'PUT') {
 
-    String? token = await storage.read(key: 'token');
+                if(request.body != '' && request.body.isNotEmpty){
+                    var body = json.decode(request.body);
+                    var type = body['type'];
+                    body.remove('type');
 
-    if (request is http.Request) {
-      if (request.method == 'POST' || request.method == 'PATCH' || request.method == 'PUT') {
+                    var data = {
+                        'type': type,
+                        'attributes': body,
+                    };
 
-        var body = json.decode(request.body);
-        var type = body['type'];
-        body.remove('type');
+                    if (request.method == 'PATCH' || request.method == 'PUT') {
+                        data['id'] = body['id'];
+                    }
 
-        var data = {
-          'type': type,
-          'attributes': body,
-        };
-
-        if (request.method == 'PATCH' || request.method == 'PUT') {
-          data['id'] = body['id'];
+                    request.body = json.encode({'data': data});
+                }
+            }
         }
-        request.body = json.encode({'data': data});
-      }
-    }
 
-    request.headers['content-type'] = 'application/vnd.api+json';
-    request.headers['accept'] = 'application/vnd.api+json';
-    if (token != null) {
-      request.headers['Authorization'] = 'Bearer $token';
-    }
+        request.headers['content-type'] = 'application/vnd.api+json';
+        request.headers['accept'] = 'application/vnd.api+json';
+        if (token != null) {
+            request.headers['Authorization'] = 'Bearer $token';
+        }
 
-    return _inner.send(request);
-  }
+        return _inner.send(request);
+    }
 }
