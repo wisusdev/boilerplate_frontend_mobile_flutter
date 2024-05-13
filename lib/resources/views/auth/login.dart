3390b3b9cc487827fa0e795fc3dd1ab86ee843fd
Jesús Avelar <wisusdev@gmail.com>
2024-05-12 18:51:57 -0600

    feat: Actualizar traducción y UI del formulario de registro

--- lib/resources/views/auth/login.dart
+++ lib/resources/views/auth/login.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:todolist_flutter/app/helpers/location.dart';
 import 'package:todolist_flutter/app/services/auth_service.dart';
@@ -15,149 +13,142 @@ class AuthLogin extends StatefulWidget {
 
 class _AuthLoginState extends State<AuthLogin> {
   	final _formKey = GlobalKey<FormState>();
-    
+
     final TextEditingController _emailController = TextEditingController();
 	final TextEditingController _passwordController = TextEditingController();
 
   	@override
   	Widget build(BuildContext context) {
+        final size = MediaQuery.of(context).size;
 
-        final sizeWith = MediaQuery.of(context).size.width;
-
-		return Scaffold(
-            appBar: AppBar(
-				title: Text(Location.of(context)!.trans('welcomeBack')),
-			),
-	  		body: Center(
-				child: Container(
-					constraints: const BoxConstraints(maxWidth: 400),
-					child: Stack(
-						children: [
-							SingleChildScrollView(
-								padding: EdgeInsets.only(top: sizeWith * 0.05, bottom: 0, left: 20, right: 20),
-								child: Form(
-									key: _formKey,
-									child: Column(
-										mainAxisAlignment: MainAxisAlignment.center,
-										crossAxisAlignment: CrossAxisAlignment.start,
-										children: [                        
-											const Row(
-												mainAxisAlignment: MainAxisAlignment.center,
-												children: [
-													Image(image: AssetImage(logoApp), width: 80, height: 80,),
-												],
-											),
-									
-											const SizedBox(height: 40),
-															
-											TextFormField(
-												controller: _emailController,
-                                                keyboardType: TextInputType.emailAddress,
-												decoration: InputDecoration(
-													labelText: Location.of(context)!.trans('email'),
-													prefixIcon: const Icon(Icons.email_outlined),
-													border: const OutlineInputBorder(),
-												),
-												validator: (value) {
-													if (value!.isEmpty) {
-                                                        return Location.of(context)!.trans('validation.emailRequired');
-                                                    }
-
-                                                    if (!RegExp(r'^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
-                                                        return Location.of(context)!.trans('validation.emailEmail');
-                                                    }
-                                                    
-                                                    return null;
+		return GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: Scaffold(
+                body: SingleChildScrollView(
+                    reverse: true,
+                    padding: EdgeInsets.only(left: 10, right: 10, top: size.height * 0.08, bottom: size.height * 0.06),
+                    child: Form(
+                        key: _formKey,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                                Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                        const Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: [
+                                                Image(image: AssetImage(logoApp), width: 80, height: 80,),
+                                            ],
+                                        ),
+                                        
+                                        const SizedBox(height: 40),
+                                        
+                                        Text(Location.of(context)!.trans('welcomeBack'), style: const TextStyle(fontSize: 31, fontWeight: FontWeight.w100)),
+                                        
+                                        const SizedBox(height: 10),
+                                        
+                                        TextFormField(
+                                            controller: _emailController,
+                                            keyboardType: TextInputType.emailAddress,
+                                            decoration: inputDecoration(labelText: Location.of(context)!.trans('email')),
+                                            validator: (value) {
+                                                if (value!.isEmpty) {
+                                                    return Location.of(context)!.trans('validation.emailRequired');
                                                 }
-                                            ),
 
-                                            const SizedBox(height: 20),
-
-                                            TextFormField(
-                                                controller: _passwordController,
-                                                keyboardType: TextInputType.visiblePassword,
-                                                decoration: InputDecoration(
-                                                    labelText: Location.of(context)!.trans('password'),
-                                                    prefixIcon: const Icon(Icons.lock_outline),
-                                                    border: const OutlineInputBorder(),
-                                                ),
-                                                obscureText: true,
-                                                validator: (value) {
-                                                    if (value!.isEmpty) {
-                                                        return 'Por favor ingresa tu contraseña';
-                                                    }
-
-                                                    if (value.length < 8) {
-                                                        return 'La contraseña debe tener al menos 8 caracteres';
-                                                    }
-
-                                                    return null;
+                                                if (!RegExp(r'^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                                                    return Location.of(context)!.trans('validation.emailEmail');
                                                 }
-                                            ),
+                                                
+                                                return null;
+                                            },
+                                        ),
 
-                                            const SizedBox(height: 30),
+                                        const SizedBox(height: 10),
 
-                                            Row(
-                                                mainAxisAlignment: MainAxisAlignment.end,
-                                                children: [
-                                                    InkWell(
-                                                        onTap: () {
-                                                            Navigator.pushNamed(context, 'forgot_password');
-                                                        },
-                                                        child: Text(Location.of(context)!.trans('forgotPassword')),
-                                                    )
-                                                ],
-                                            ),
+                                        TextFormField(
+                                            controller: _passwordController,
+                                            keyboardType: TextInputType.visiblePassword,
+                                            obscureText: true,
+                                            decoration: inputDecoration(labelText: Location.of(context)!.trans('password')),
+                                            validator: (value) {
+                                                if (value!.isEmpty) {
+                                                    return 'Por favor ingresa tu contraseña';
+                                                }
 
-                                            const SizedBox(height: 30),
+                                                if (value.length < 8) {
+                                                    return 'La contraseña debe tener al menos 8 caracteres';
+                                                }
 
-                                            ElevatedButton(
-                                                style: ElevatedButton.styleFrom(
-                                                    backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                                                    foregroundColor: Theme.of(context).colorScheme.primary,
-                                                    minimumSize: const Size.fromHeight(45),
-                                                    shape: RoundedRectangleBorder(
-                                                        borderRadius: BorderRadius.circular(20),
-                                                    ),
+                                                return null;
+                                            }
+                                        ),
+
+                                        const SizedBox(height: 20),
+                                        
+                                        Row(
+                                            mainAxisAlignment: MainAxisAlignment.end,
+                                            children: [
+                                                InkWell(
+                                                    onTap: () {
+                                                        Navigator.pushNamed(context, 'forgot_password');
+                                                    },
+                                                    child: Text(Location.of(context)!.trans('forgotPassword'), style: const TextStyle(fontWeight: FontWeight.w500, fontFamily: 'Sofia')),
                                                 ),
-                                                onPressed: () {
-                                                    if (_formKey.currentState!.validate()) {
-                                                        _formKey.currentState!.save();
+                                            ],
+                                        ),
+                                        
+                                        const SizedBox(height: 40),
 
-                                                        login(context);
-
-                                                        getScafoldMessage(context, 'Procesando datos...');
-                                                    }
-                                                },
-                                                child: Text(Location.of(context)!.trans('login'), style: const TextStyle(fontSize: 16))
+                                        ElevatedButton(
+                                            style: ElevatedButton.styleFrom(
+                                                backgroundColor: Color(Theme.of(context).colorScheme.primary.value),
+                                                padding: const EdgeInsets.all(13),
+                                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
                                             ),
+                                            child: Center(
+                                                child: Text(Location.of(context)!.trans('login'), style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontFamily: "Sofia",
+                                                    color: Color(Theme.of(context).colorScheme.onPrimary.value),
+                                                    fontSize: 18.0
+                                                ))
+                                            ),
+                                            onPressed: () {
+                                                if (_formKey.currentState!.validate()) {
+                                                    _formKey.currentState!.save();
 
-                                            const SizedBox(height: 10),
-                                        ],
-                                    ),
+                                                    login(context);
+                                                }
+                                            },
+                                        ),
+                                    ],
                                 ),
-                            ),
 
-                            Positioned(
-                                child: Container(
-                                    padding: EdgeInsets.only(top: sizeWith * 1.30),
-                                    child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                            Text(Location.of(context)!.trans('dontHaveAccount')),
-                                            InkWell(
-                                                onTap: () {
-                                                    Navigator.pushNamed(context, 'register');
-                                                },
-                                                child: Text(' ${Location.of(context)!.trans('registerHere')}', style: const TextStyle(fontWeight: FontWeight.bold)),
-                                            ),
-                                        ],
-                                    ),
+                                const SizedBox(height: 40),
+                                
+                                Column(
+                                    children: [
+                                        Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: [
+                                                InkWell(
+                                                    onTap: () {
+                                                        Navigator.pushNamed(context, 'register');
+                                                    },
+                                                    child: Text(Location.of(context)!.trans('registerHere'), style: const TextStyle(fontFamily: 'Sofia'))
+                                                ),
+                                            ],
+                                        ),
+                                    ],
                                 )
-                            )
-                        ]
+                            ],
+                        ),
                     ),
-                ),
+                )
             ),
         );
     }
@@ -177,4 +168,14 @@ class _AuthLoginState extends State<AuthLogin> {
             getScafoldMessage(context, 'Error al iniciar sesión');
         }
     }
+}
+
+InputDecoration inputDecoration({required String labelText}){
+    return InputDecoration(
+        labelText: labelText,
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: const BorderSide(color: Colors.transparent)
+        ),
+    );
 }
