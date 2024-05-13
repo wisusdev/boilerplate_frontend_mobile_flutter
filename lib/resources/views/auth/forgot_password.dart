e4dbc0bcc8289aeac2415a4c4e21f846689cf215
Jesús Avelar <wisusdev@gmail.com>
2024-05-12 18:51:48 -0600

    feat: Actualizar traducción y UI del formulario de registro

--- lib/resources/views/auth/forgot_password.dart
+++ lib/resources/views/auth/forgot_password.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todolist_flutter/app/helpers/location.dart';
 import 'package:todolist_flutter/app/services/auth_service.dart';
+import 'package:todolist_flutter/config/app.dart';
 import 'package:todolist_flutter/resources/widgets/snack_bar.dart';
 
 class AuthForgotPassword extends StatefulWidget {
@@ -11,101 +12,114 @@ class AuthForgotPassword extends StatefulWidget {
 }
 
 class _AuthForgotPasswordState extends State<AuthForgotPassword> {
-	final _formKey = GlobalKey<FormState>();
+	final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  	
     final TextEditingController _emailController = TextEditingController();
   	
   	@override
   	Widget build(BuildContext context) {
-		final sizeWith = MediaQuery.of(context).size.width;
+		final size = MediaQuery.of(context).size;
 
-    	return Scaffold(
-      		appBar: AppBar(
-        		title: const Text('Forgot Password'),
-      		),
-      		body: Center(
-                child: Container(
-                    constraints: const BoxConstraints(maxWidth: 400),
-                    child: Padding(
-                        padding: EdgeInsets.only(top: sizeWith * 0.05, bottom: 0, left: 20, right: 20),
-                        child: Form(
-                            key: _formKey,
-                            child: Column(
-                                //mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                    const Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                            Image(
-                                                image: AssetImage('lib/assets/images/wisus-logo.png'), 
-                                                width: 80,
-                                                height: 80,
-                                            ),
-                                        ],
-                                    ),
-
-                                    const SizedBox(height: 20),
-
-                                    Text(Location.of(context)!.trans('forgotPasswordInstructions'), style: const TextStyle( fontSize: 16)),
-
-                                    const SizedBox(height: 20),
-
-                                    TextFormField(
-                                        controller: _emailController,
-                                        decoration: InputDecoration(
-                                            labelText: Location.of(context)!.trans('email'),
-                                            prefixIcon: const Icon(Icons.email_outlined),
-                                            border: const OutlineInputBorder(),
+    	return GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: Scaffold(
+                body: SingleChildScrollView(
+                    reverse: true,
+                    padding: EdgeInsets.only(left: 10, right: 10, top: size.height * 0.08, bottom: size.height * 0.06),
+                    child: Form(
+                        key: _formKey,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                                Column(
+                                    children: [
+                                        const Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: [
+                                                Image(image: AssetImage(logoApp), width: 80, height: 80,),
+                                            ],
                                         ),
-                                        validator: (value) {
-                                            if (value!.isEmpty) {
-												return 'Por favor ingresa tu correo electrónico';
-											}
 
-											if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
-											    return 'Por favor ingresa un correo electrónico válido';
-											}
+                                        const SizedBox(height: 20),
 
-											return null;
-                                        },
-                                    ),
+                                        Text(Location.of(context)!.trans('resetPassword'), style: const TextStyle( fontSize: 31, fontWeight: FontWeight.w100)),
 
-                                    const SizedBox(height: 30),
+                                        const SizedBox(height: 20),
 
-                                    ElevatedButton(
-                                        style: ElevatedButton.styleFrom(
-                                            backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                                            foregroundColor: Theme.of(context).colorScheme.primary,
-                                            minimumSize: const Size.fromHeight(45),
-                                            shape: RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.circular(20),
+                                        TextFormField(
+                                            keyboardType: TextInputType.emailAddress,
+                                            decoration: InputDecoration(
+                                                labelText: Location.of(context)!.trans('email'),
+                                                border: OutlineInputBorder(
+                                                    borderRadius: BorderRadius.circular(20),
+                                                    borderSide: const BorderSide(color: Colors.transparent)
+                                                ),
                                             ),
-                                        ),
-                                        onPressed: (){
-                                            if (_formKey.currentState!.validate()) {
-                                                _formKey.currentState!.save();
-
-                                                forgotPassword(context);
+                                            validator: (value) {
+                                                if (value!.isEmpty) {
+                                                return 'Por favor ingresa tu correo electrónico';
                                             }
-                                        },
-                                        child: Text(Location.of(context)!.trans('resetPwd')),
-                                    ),
-                                ],
-                            ),
+
+                                            if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                                                return 'Por favor ingresa un correo electrónico válido';
+                                            }
+
+                                            return null;
+                                            },
+                                        ),
+
+                                        const SizedBox(height: 40),
+
+                                        ElevatedButton(
+                                            style: ElevatedButton.styleFrom(
+                                                backgroundColor: Color(Theme.of(context).colorScheme.primary.value),
+                                                padding: const EdgeInsets.all(13),
+                                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                                            ),
+                                            child: Center(
+                                                child: Text(Location.of(context)!.trans('sendPasswordResetLink'), style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontFamily: "Sofia",
+                                                    color: Color(Theme.of(context).colorScheme.onPrimary.value),
+                                                    fontSize: 18.0
+                                                ))
+                                            ),
+                                            onPressed: (){
+                                                if (_formKey.currentState!.validate()) {
+                                                    _formKey.currentState!.save();
+
+                                                    forgotPassword(context);
+                                                }
+                                            },
+                                        ),
+                                    ],
+                                ),
+                                const SizedBox(height: 40),
+                                Column(
+                                    children: [
+                                        Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: [
+                                                InkWell(
+                                                    onTap: () {
+                                                        Navigator.pushNamed(context, 'login');
+                                                    },
+                                                    child: Text(Location.of(context)!.trans('login'), style: const TextStyle(fontFamily: 'Sofia'))
+                                                )
+                                            ],
+                                        )
+                                    ],
+                                )
+                            ],
                         ),
                     ),
                 ),
             ),
-    	);
+        );
   	}
 
     void forgotPassword(context) async {
-
-        setState(() {
-        });
-
         Map<String, dynamic> data = {
             'email': _emailController.text,
         };
@@ -117,8 +131,5 @@ class _AuthForgotPasswordState extends State<AuthForgotPassword> {
         } else {
             getScafoldMessage(context, response['message']);
         }
-
-        setState(() {
-        });
     }
 }
