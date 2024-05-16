153c729ef463234bbb691dc806a38c1513677d09
Jesús Avelar <wisusdev@gmail.com>
2024-05-15 23:08:52 -0600

    Refactorizar funciones de texto en el archivo text.dart

--- lib/app/helpers/text.dart
+++ lib/app/helpers/text.dart
@@ -2,6 +2,30 @@ String capitalizeText(String text) {
     if (text.isEmpty) {
         return text;
     }
+    
+    return text[0].toUpperCase() + text.substring(1).toLowerCase();
+}
+
+String pluralizeText(String text) {
+    if (text.isEmpty) {
+        return text;
+    }
   
-    return text[0].toUpperCase() + text.substring(1);
+    return '${text}s';
+}
+
+String singularizeText(String text) {
+    if (text.isEmpty) {
+        return text;
+    }
+  
+    return text.substring(0, text.length - 1);
+}
+
+String toUpperCaseText(String text) {
+    return text.toUpperCase();
+}
+
+String toLowerCaseText(String text) {
+    return text.toLowerCase();
 }
