da5013aa241d622c3ba7a96f1432093bb3868857
Jesús Avelar <wisusdev@gmail.com>
2025-01-11 23:23:24 -0600

    feat: Añadir formulario para cambiar contraseña con validación y manejo de errores

--- lib/resources/views/account/change_password.dart
+++ lib/resources/views/account/change_password.dart
@@ -1,3 +1,6 @@
+import 'package:boilerplate_frontend_mobile_flutter/app/helpers/error_manager.dart';
+import 'package:boilerplate_frontend_mobile_flutter/app/helpers/location.dart';
+import 'package:boilerplate_frontend_mobile_flutter/resources/widgets/input_decoration.dart';
 import 'package:flutter/material.dart';
 
 class ChangePassword extends StatefulWidget {
@@ -8,6 +11,19 @@ class ChangePassword extends StatefulWidget {
 }
 
 class _ChangePasswordState extends State<ChangePassword> {
+    final _formKey = GlobalKey<FormState>();
+
+    final TextEditingController _currentPasswordController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+    final TextEditingController _passwordConfirmationController = TextEditingController();
+    bool _isLoading = false;
+
+    final ErrorManager errorManager = ErrorManager(initialErrors: {
+        'current_password': null,
+        'password': null,
+        'password_confirmation': null,
+    });
+
     @override
     Widget build(BuildContext context) {
         return Scaffold(
@@ -18,10 +34,102 @@ class _ChangePasswordState extends State<ChangePassword> {
             ),
 
             body: SingleChildScrollView(
-                child: Column(
-                    children: [
-                        Text('Change Password'),
-                    ],
+                child: Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                    child: Form(
+                        key: _formKey,
+                        child: Column(
+                            children: [
+
+                                TextFormField(
+                                    controller: _currentPasswordController,
+                                    decoration: inputDecorationStyle(labelText: 'Current Password'),
+                                    obscureText: true,
+                                    validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                            return 'Please enter your current password';
+                                        }
+
+                                        if (errorManager.errors['current_password'] != null) {
+                                            return errorManager.errors['current_password'];
+                                        }
+
+                                        return null;
+                                    },
+                                ),
+
+                                const SizedBox(height: 20),
+
+                                TextFormField(
+                                    controller: _passwordController,
+                                    keyboardType: TextInputType.visiblePassword,
+                                    obscureText: true,
+                                    decoration: inputDecorationStyle(labelText: Location.of(context)!.trans('password')),
+                                    validator: (value) {
+                                        if (value!.isEmpty) {
+                                            return 'Por favor ingresa tu contraseña';
+                                        }
+
+                                        if (value.length < 8) {
+                                            return 'La contraseña debe tener al menos 8 caracteres';
+                                        }
+
+                                        if(errorManager.errors['password'] != null){
+                                            return errorManager.errors['password'];
+                                        }
+
+                                        return null;
+                                    }
+                                ),
+
+                                const SizedBox(height: 20),
+
+                                TextFormField(
+                                    controller: _passwordConfirmationController,
+                                    decoration: inputDecorationStyle(labelText: 'Confirm New Password'),
+                                    obscureText: true,
+                                    validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                            return 'Please confirm your new password';
+                                        }
+                                        
+                                        if (value != _passwordController.text) {
+                                            return 'Passwords do not match';
+                                        }
+
+                                        if (errorManager.errors['password_confirmation'] != null) {
+                                            return errorManager.errors['password_confirmation'];
+                                        }
+
+                                        return null;
+                                    },
+                                ),
+                                
+                                const SizedBox(height: 20),
+
+                                ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        backgroundColor: Theme.of(context).colorScheme.primary,
+                                        padding: const EdgeInsets.all(13),
+                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                                    ),
+                                    onPressed: () {
+                                        if (_formKey.currentState!.validate()) {
+                                            // Process data
+                                        }
+                                    },
+                                    child: Center(
+                                        child: Text(Location.of(context)!.trans('save'), style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontFamily: "Sofia",
+                                            color: Theme.of(context).colorScheme.onPrimary,
+                                            fontSize: 18.0
+                                        ))
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
                 ),
             ),
         );
