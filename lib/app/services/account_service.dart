010a795683a56dc9f1d0b6b19d0c6287d5d5f363
Jesús Avelar <wisusdev@gmail.com>
2025-01-13 08:18:37 -0600

    feat: Añadir método para obtener la lista de autenticación de dispositivos en AccountService

--- lib/app/services/account_service.dart
+++ lib/app/services/account_service.dart
@@ -10,12 +10,14 @@ class AccountService {
     final String _apiUri;
     late String _apiUriAccount;
     late String _apiUriChangePassword;
+    late String _apiUriDeviceAuthList;
 
     final ApiInterceptor client = ApiInterceptor();
 
     AccountService(): _apiUri = apiUrlV1 {
         _apiUriAccount = '$_apiUri/account/profile';
         _apiUriChangePassword = '$_apiUri/account/change-password';
+        _apiUriDeviceAuthList = '$_apiUri/account/device-auth-list';
     }
 
     updateProfile({required Map<String, String> data}) async {
@@ -37,6 +39,22 @@ class AccountService {
         var uri = Uri.parse(_apiUriChangePassword);
 
         Response accountResponse = await client.patch(uri, body: json.encode(data));
+
+        final Map<String, dynamic> responseBody = json.decode(accountResponse.body);
+
+        return responseBody;
+    }
+
+    getDeviceAuthList({required Map<String, String> data}) async {
+
+        Map<String, dynamic> params = {
+            'fields[device_infos]': 'id,login_at,browser,os,ip,country',
+            'page[number]': '1',
+        };
+
+        var uri = Uri.parse('$_apiUriDeviceAuthList?${Uri(queryParameters: params).query}');
+
+        Response accountResponse = await client.get(uri);
 
         final Map<String, dynamic> responseBody = json.decode(accountResponse.body);
 
