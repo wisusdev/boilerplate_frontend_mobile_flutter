bcdb6f3b53013c4b608cbd92080d7a3afbc5b10f
Jesús Avelar <wisusdev@gmail.com>
2025-01-14 00:10:50 -0600

    feat: Renombrar cliente HTTP en ApiInterceptor para mayor claridad

--- lib/app/interceptors/api_interceptor.dart
+++ lib/app/interceptors/api_interceptor.dart
@@ -4,7 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiInterceptor extends http.BaseClient {
 
-    final http.Client _inner = http.Client();
+    final http.Client _httpClient = http.Client();
 
     @override
     Future<http.StreamedResponse> send(http.BaseRequest request) async {
@@ -50,6 +50,6 @@ class ApiInterceptor extends http.BaseClient {
             request.headers['Authorization'] = 'Bearer $token';
         }
 
-        return _inner.send(request);
+        return _httpClient.send(request);
     }
 }
