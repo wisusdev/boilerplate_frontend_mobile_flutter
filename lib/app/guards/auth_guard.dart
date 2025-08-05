8e6606cb688df95fd3386e20b68d844268016243
Jesús Avelar <wisusdev@gmail.com>
2025-08-04 22:40:26 -0600

    refactor: Simplificar la implementación del AuthGuard y mejorar la legibilidad del código

--- lib/app/guards/auth_guard.dart
+++ lib/app/guards/auth_guard.dart
@@ -1,35 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:boilerplate_frontend_mobile_flutter/app/providers/auth_provider.dart';
 import 'package:boilerplate_frontend_mobile_flutter/resources/views/auth/login.dart';
 
 class AuthGuard extends StatelessWidget {
-	final Widget child;
+    final Widget child;
 
-	const AuthGuard({Key? key, required this.child}) : super(key: key);
+    const AuthGuard({Key? key, required this.child}) : super(key: key);
 
-	@override
-	Widget build(BuildContext context)  {
-		final authService = Provider.of<AuthProvider>(context);
+    @override
+    Widget build(BuildContext context)  {
+        final AuthProvider authProvider = AuthProvider();
 
-		return FutureBuilder<bool>(
-	  		future: authService.isUserAuthenticated(),
-	  		builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-				if (snapshot.connectionState == ConnectionState.waiting) {
-					return const CircularProgressIndicator();
-				} else if (snapshot.hasData && snapshot.data == true) {
-					return child;
-				} else {
-					WidgetsBinding.instance.addPostFrameCallback((_) {
-							Navigator.of(context).pushAndRemoveUntil(
-								MaterialPageRoute(builder: (context) => const AuthLogin()),
-								(Route<dynamic> route) => false,
-							);
-					});
-                    
-					return Container(); // Return an empty container while the navigation happens
-				}
-	  		},
-		);
-	}
+        return FutureBuilder<bool>(
+            future: authProvider.isUserAuthenticated(),
+            builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                } else if (snapshot.hasData && snapshot.data == true) {
+                    return child;
+                } else {
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+                        Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(builder: (context) => const AuthLogin()),
+                            (Route<dynamic> route) => false,
+                        );
+                    });
+
+                    return Container(); // Return an empty container while the navigation happens
+                }
+            },
+        );
+    }
 }
