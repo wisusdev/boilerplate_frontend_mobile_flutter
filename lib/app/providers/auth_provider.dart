27cd72668f87b12e8f40ffdfc7934e501d265c17
Jesús Avelar <wisusdev@gmail.com>
2025-08-04 22:44:06 -0600

    refactor: Eliminar la herencia de ChangeNotifier en AuthProvider para simplificar la clase

--- lib/app/providers/auth_provider.dart
+++ lib/app/providers/auth_provider.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class AuthProvider extends ChangeNotifier {
-
+class AuthProvider {
     Future<bool> isUserAuthenticated() async {
         SharedPreferences prefs = await SharedPreferences.getInstance();
         String? token = prefs.getString('access_token');
