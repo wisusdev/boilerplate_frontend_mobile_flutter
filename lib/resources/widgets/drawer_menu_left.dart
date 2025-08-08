237084420fdd82dbcbc3ff60489299e8aa7e6ea5
Jesús Avelar <wisusdev@gmail.com>
2025-08-07 21:49:53 -0600

    refactor: Simplificar el código del menú de navegación izquierdo y mejorar la legibilidad

--- lib/resources/widgets/drawer_menu_left.dart
+++ lib/resources/widgets/drawer_menu_left.dart
@@ -4,46 +4,44 @@ import 'package:boilerplate_frontend_mobile_flutter/app/helpers/text.dart';
 import 'package:boilerplate_frontend_mobile_flutter/app/utils/menu.dart';
 
 class DrawerMenuLeft extends StatefulWidget {
-    final GlobalKey<ScaffoldState> scaffoldKey;
+  final GlobalKey<ScaffoldState> scaffoldKey;
 
-    const DrawerMenuLeft({super.key, required this.scaffoldKey});
+  const DrawerMenuLeft({super.key, required this.scaffoldKey});
 
-    @override
-    State<DrawerMenuLeft> createState() => _DrawerMenuLeftState();
+  @override
+  State<DrawerMenuLeft> createState() => _DrawerMenuLeftState();
 }
 
 class _DrawerMenuLeftState extends State<DrawerMenuLeft> {
-    int _selectedIndex = 0;
+  int _selectedIndex = 0;
 
-    @override
-    Widget build(BuildContext context) {
-        final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+  @override
+  Widget build(BuildContext context) {
+    return NavigationDrawer(
+      selectedIndex: _selectedIndex,
 
-        return NavigationDrawer(
-            selectedIndex: _selectedIndex,
-            
-            onDestinationSelected: (index) { 
-                setState(() {
-                    _selectedIndex = index;
-                });
-
-                final menuItem = appMenuItems[index];
-                Navigator.of(context).pushNamed(menuItem.link);
-                Scaffold.of(context).closeDrawer();
-                //widget.scaffoldKey.currentState?.closeDrawer();
-            },
-
-            children: [
-                Padding(
-                    padding: EdgeInsets.fromLTRB(28, hasNotch ? 0 : 20, 16, 10),
-                    child: Text(capitalizeText(Location.of(context)!.trans('home'))),
-                ),
-
-                ...appMenuItems.map((item) => NavigationDrawerDestination(
-                    icon: Icon(item.icon), 
-                    label: Text(capitalizeText(Location.of(context)!.trans(item.title))),
-                ))
-            ],
-        );
-    }
+      onDestinationSelected: (index) {
+        setState(() {
+          _selectedIndex = index;
+        });
+        final menuItem = appMenuItems[index];
+        Navigator.of(context).pushNamed(menuItem.link);
+        widget.scaffoldKey.currentState?.closeDrawer();
+      },
+      children: [
+        NavigationDrawerDestination(
+          icon: Icon(appMenuItems[0].icon),
+          label: Text(capitalizeText(Location.of(context)!.trans(appMenuItems[0].title))),
+        ),
+        NavigationDrawerDestination(
+          icon: Icon(appMenuItems[1].icon),
+          label: Text(capitalizeText(Location.of(context)!.trans(appMenuItems[1].title))),
+        ),
+        NavigationDrawerDestination(
+          icon: Icon(appMenuItems[2].icon),
+          label: Text(capitalizeText(Location.of(context)!.trans(appMenuItems[2].title))),
+        ),
+      ],
+    );
+  }
 }
