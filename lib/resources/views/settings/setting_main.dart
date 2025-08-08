b17cdec299d5a95a45022983353432e784cbd54c
Jesús Avelar <wisusdev@gmail.com>
2025-08-07 21:49:31 -0600

    refactor: Simplificar la estructura del widget SettingMain y mejorar la legibilidad del código

--- lib/resources/views/settings/setting_main.dart
+++ lib/resources/views/settings/setting_main.dart
@@ -3,6 +3,7 @@ import 'package:boilerplate_frontend_mobile_flutter/app/helpers/location.dart';
 import 'package:boilerplate_frontend_mobile_flutter/app/helpers/text.dart';
 
 class SettingMain extends StatefulWidget {
+   static const String title = 'settings';
   const SettingMain({Key? key}) : super(key: key);
 
   @override
@@ -12,52 +13,54 @@ class SettingMain extends StatefulWidget {
 class _SettingMainState extends State<SettingMain> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(capitalizeText(Location.of(context)!.trans('settings')), style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
-        iconTheme: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-      ),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          Container(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-            child: Column(children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 20),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Theme.of(context).colorScheme.onSecondary,
-                  boxShadow: [
-                    BoxShadow(color: Theme.of(context).colorScheme.shadow, offset: const Offset(0, 1), blurRadius: 3),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: Icon(Icons.translate, color: Theme.of(context).colorScheme.primary),
-                      title: Text(capitalizeText(Location.of(context)!.trans('language')), style: const TextStyle(fontSize: 16)),
-                      trailing: const Icon(Icons.arrow_forward_ios_rounded),
-                      onTap: () {
-                        Navigator.of(context).pushNamed('language');
-                      },
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.palette, color: Theme.of(context).colorScheme.primary),
-                      title: Text(capitalizeText(Location.of(context)!.trans('theme')), style: const TextStyle(fontSize: 16)),
-                      trailing: const Icon(Icons.arrow_forward_ios_rounded),
-                      onTap: () {
-                        Navigator.of(context).pushNamed('theme');
-                      },
-                    ),
-                  ],
-                ),
-              )
-            ]),
-          ),
-        ]),
-      ),
+    return SingleChildScrollView(
+      child: Column(children: [
+        Container(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+          child: Column(children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 20),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Theme.of(context).colorScheme.onSecondary,
+                boxShadow: [
+                  BoxShadow(
+                      color: Theme.of(context).colorScheme.shadow,
+                      offset: const Offset(0, 1),
+                      blurRadius: 3),
+                ],
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.translate,
+                        color: Theme.of(context).colorScheme.primary),
+                    title: Text(
+                        capitalizeText(Location.of(context)!.trans('language')),
+                        style: const TextStyle(fontSize: 16)),
+                    trailing: const Icon(Icons.arrow_forward_ios_rounded),
+                    onTap: () {
+                      Navigator.of(context).pushNamed('language');
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.palette,
+                        color: Theme.of(context).colorScheme.primary),
+                    title: Text(
+                        capitalizeText(Location.of(context)!.trans('theme')),
+                        style: const TextStyle(fontSize: 16)),
+                    trailing: const Icon(Icons.arrow_forward_ios_rounded),
+                    onTap: () {
+                      Navigator.of(context).pushNamed('theme');
+                    },
+                  ),
+                ],
+              ),
+            )
+          ]),
+        ),
+      ]),
     );
   }
 }
