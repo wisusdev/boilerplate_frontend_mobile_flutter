cf5b0307767d30dbc962c44c30a6309dd6f8443f
Jesús Avelar <wisusdev@gmail.com>
2025-08-07 21:49:38 -0600

    refactor: Eliminar código comentado y simplificar la estructura de HomeView

--- lib/resources/views/home.dart
+++ lib/resources/views/home.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:boilerplate_frontend_mobile_flutter/app/helpers/location.dart';
-import 'package:boilerplate_frontend_mobile_flutter/app/helpers/text.dart';
 import 'package:boilerplate_frontend_mobile_flutter/app/preferences/theme_preferences.dart';
 
 class HomeView extends StatefulWidget {
+  static const String title = 'home';
   const HomeView({Key? key}) : super(key: key);
 
   @override
@@ -11,38 +10,14 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-    @override
-    Widget build(BuildContext context) {
-
-        final scaffoldKey = GlobalKey<ScaffoldState>();
-
-        return Scaffold(
-            key: scaffoldKey,
-            
-            appBar: AppBar(
-                title: Text(
-                    capitalizeText(Location.of(context)!.trans('home')),
-                    style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)
-                ),
-                iconTheme: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-                backgroundColor: Theme.of(context).colorScheme.primary,
-            ),
-
-            body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                    Text('isDarkmode: ${ ThemePreferences.getThemeMode() }'),
-                    const Divider(),
-                ],
-            ),
-
-            /*floatingActionButton: FloatingActionButton(
-                onPressed: () {},
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                child: Icon(Icons.qr_code_scanner, color: Theme.of(context).colorScheme.onPrimary),
-            ),*/
-
-            //drawer: DrawerMenuLeft(scaffoldKey : scaffoldKey),
-        );
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('isDarkmode: ${ThemePreferences.getThemeMode()}'),
+        const Divider(),
+      ],
+    );
   }
 }
