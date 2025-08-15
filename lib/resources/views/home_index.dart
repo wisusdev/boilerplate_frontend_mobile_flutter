6acc32a596e8e57ec47951a4fe87d04f9d62277b
Jesús Avelar <wisusdev@gmail.com>
2025-08-15 12:08:44 -0600

    refactor: Eliminar HomeView y crear HomeIndex para mejorar la estructura de la vista de inicio

---
+++ lib/resources/views/home_index.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:boilerplate_frontend_mobile_flutter/app/preferences/theme_preferences.dart';
+
+class HomeIndex extends StatefulWidget {
+  const HomeIndex({Key? key}) : super(key: key);
+
+  @override
+  State<HomeIndex> createState() => _HomeIndexState();
+}
+
+class _HomeIndexState extends State<HomeIndex> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('isDarkmode: ${ThemePreferences.getThemeMode()}'),
+        const Divider(),
+      ],
+    );
+  }
+}
