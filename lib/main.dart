9067159e05c67714753b3755f73f61234bb17f02
Jesús Avelar <wisusdev@gmail.com>
2025-08-14 22:23:12 -0600

    refactor: Reorganizar importaciones y mejorar la estructura del main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,63 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-import 'package:provider/provider.dart';
 import 'package:boilerplate_frontend_mobile_flutter/app/guards/auth_guard.dart';
+import 'package:boilerplate_frontend_mobile_flutter/resources/views/home.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'package:boilerplate_frontend_mobile_flutter/config/app.dart';
+import 'package:boilerplate_frontend_mobile_flutter/config/languages.dart';
+import 'package:boilerplate_frontend_mobile_flutter/routes/api_routes.dart';
+import 'package:boilerplate_frontend_mobile_flutter/app/helpers/local_storage.dart';
 import 'package:boilerplate_frontend_mobile_flutter/app/preferences/language_preferences.dart';
 import 'package:boilerplate_frontend_mobile_flutter/app/preferences/theme_preferences.dart';
 import 'package:boilerplate_frontend_mobile_flutter/app/providers/language_provider.dart';
-import 'package:boilerplate_frontend_mobile_flutter/config/app.dart';
-import 'package:boilerplate_frontend_mobile_flutter/config/languages.dart';
-import 'package:boilerplate_frontend_mobile_flutter/app/helpers/local_storage.dart';
 import 'package:boilerplate_frontend_mobile_flutter/app/providers/theme_provider.dart';
 import 'package:boilerplate_frontend_mobile_flutter/app/helpers/location_delegate.dart';
 import 'package:boilerplate_frontend_mobile_flutter/resources/themes/dark_theme.dart';
 import 'package:boilerplate_frontend_mobile_flutter/resources/themes/light_theme.dart';
-import 'package:boilerplate_frontend_mobile_flutter/resources/views/layouts/app.dart';
-import 'package:boilerplate_frontend_mobile_flutter/routes/api_routes.dart';
 
 void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await LocalStorage.init();
+  WidgetsFlutterBinding.ensureInitialized();
 
-    await dotenv.load(fileName: ".env");
+  await LocalStorage.init();
 
-  	runApp(
-        MultiProvider(
-            providers: [
-                ChangeNotifierProvider(create: (context) => ThemeProvider(themeMode: ThemePreferences.getThemeMode())),
-                ChangeNotifierProvider(create: (context) => LanguageProvider(languageLocale: Locale(LanguagePreferences.getLanguageMode()))),
-            ], 
-            child: const MyApp()
-        ),
-    );
+  await dotenv.load(fileName: ".env");
+
+  runApp(
+    MultiProvider(providers: [
+      ChangeNotifierProvider(create: (context) => ThemeProvider(themeMode: ThemePreferences.getThemeMode())),
+      ChangeNotifierProvider(create: (context) => LanguageProvider(languageLocale: Locale(LanguagePreferences.getLanguageMode()))),
+    ], child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+  const MyApp({super.key});
 
-  	@override
-  	Widget build(BuildContext context) {
-        return MaterialApp(
-            // Titulo de la app
-            title: appName,
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appName,
 
-            // Desactivar el banner de debug
-            debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
 
-            // Soporte para idiomas
-            supportedLocales: supportedLocales,
-            locale: Provider.of<LanguageProvider>(context).language,
-        
-            localizationsDelegates: const [
-                LocationDelegate(),
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-            ],
+      supportedLocales: supportedLocales,
+      locale: Provider.of<LanguageProvider>(context).language,
 
-            // Soporte para temas
-            theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
-            darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
-            themeMode: Provider.of<ThemeProvider>(context).themeMode,
+      localizationsDelegates: const [
+        LocationDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
 
-            // Rutas
-            home: const AuthGuard(child: AppLayout()),
-            routes: api,
-        );
-    }
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      themeMode: Provider.of<ThemeProvider>(context).themeMode,
+
+      home: const AuthGuard(child: HomeView()),
+      routes: api,
+    );
+  }
 }
