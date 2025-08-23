20d90e782467efb9724cc7eeedac0d5c994044cb
Jesús Avelar <wisusdev@gmail.com>
2025-08-22 21:21:31 -0600

    refactor: Mejorar la gestión de permisos en PermissionIndex y actualizar la interfaz de usuario

--- lib/resources/views/base/permissions/permission_index.dart
+++ lib/resources/views/base/permissions/permission_index.dart
@@ -1,3 +1,5 @@
+import 'package:boilerplate_frontend_mobile_flutter/app/data/models/permissions_model.dart';
+import 'package:boilerplate_frontend_mobile_flutter/app/http/controllers/permission_controller.dart';
 import 'package:flutter/material.dart';
 
 class PermissionIndex extends StatefulWidget {
@@ -8,10 +10,43 @@ class PermissionIndex extends StatefulWidget {
 }
 
 class _PermissionIndexState extends State<PermissionIndex> {
+
+  final PermissionController _permissionController = PermissionController();
+  List<PermissionAttribute> _permissions = [];
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  @override
+  void initState() {
+    super.initState();
+    _getPermissions();
+  }
+
+  Future<void> _getPermissions() async {
+    _permissionController.permissionIndex(context).then((permissions) {
+      setState(() {
+        _permissions = permissions.data.attributes;
+        _isLoading = false;
+        _errorMessage = null;
+      });
+    }).catchError((error) {
+      setState(() {
+        _isLoading = false;
+        _errorMessage = 'Failed to load permissions';
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Permissions Index Page'),
+    return  _isLoading ? const Center(child: CircularProgressIndicator()) : _errorMessage != null ? Center(child: Text(_errorMessage!)) : ListView.builder(
+      itemCount: _permissions.length,
+      itemBuilder: (context, index) {
+        final permission = _permissions[index];
+        return ListTile(
+          title: Text(permission.name),
+        );
+      },
     );
   }
 }
